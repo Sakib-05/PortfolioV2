cf6ee3a23ee3641fed3172c5bad9a52efd4a8e30
sakibhoque <sakibhoque@Sakibs-MacBook-Pro.local>
2024-11-02 21:21:18 +0000

    Began development of the Banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,15 @@
 export default function Banner() {
     return (
         <div className="Banner">
-            <h1>this is the banner section</h1>
+            <div className="introduction">
+                <h2>welcome to my portfolio website</h2>
+                <h1>Hi! I'm Sakib</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium commodi magni soluta corporis animi aliquam nihil perferendis ad atque aperiam molestiae, nobis, vero placeat tempora sequi in exercitationem iusto veniam.</p>
+            </div>
+            <div className="temp">
+                <p>image for later</p>
+
+            </div>
         </div>
     );
     
