ac0c35f64e1d21828074d1aa3abb6c6dd90badea
sakibhoque <sakibhoque@Sakibs-MacBook-Pro.local>
2024-11-02 20:41:53 +0000

    finished NavBar and improved layout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
+
 export default function NavBar() {
+    const [theme, setTheme] = useState(true);
+    let dark = "./dark icon.png";
+    let light = "./light icon.webp";
+
     return(
         <div className="NavBar">
-            <h1>Logo</h1>
+            <h2>MY PORTFOLIO</h2>
 
             <section>
-                <img src="" alt="light/dark toggle" />
-                <button>Resume</button>
+                {theme ? (<img onClick={()=>setTheme(!theme)} src={dark} alt="dark" />):(<img onClick={()=>setTheme(!theme)} src={light} alt="light" />) }
+                <a href="CV.pdf" target="_blank">resume</a>
             </section>
 
         </div>
