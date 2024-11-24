08e05a270d778c8e0ff7cb9fbe5dcd03b3fc90cf
sakibhoque <sakibhoque@Sakibs-MacBook-Pro.local>
2024-11-24 23:34:21 +0000

    began filling in the Skills section. Added each Skills category and filled them with corresponding skills. Need to add logo images of each skill

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,94 @@
 export default function Skills() {
     return(
         <div className="Skills">
-            <h1>this is the skills section</h1>
+            <section>
+                <h1>programming languages</h1>
+                <br />
+                <br />
+                <div className="icons-container">
+                    <div className="Skills-icons" title="Java">
+                        <img src="Images/Java logo.webp" alt="Java"  />
+                        <h5>Java</h5>
+                    </div>
+
+                    <div className="Skills-icons" title="JavaScript">
+                        <img src="Images/Java logo.webp" alt="JavaScript"  />
+                        <h5>JavaScript</h5>
+                    </div>
+
+                    <div className="Skills-icons" title="Python">
+                        <img src="Images/Java logo.webp" alt="Python"  />
+                        <h5>Python</h5>
+                    </div>
+
+                    <div className="Skills-icons" title="C++">
+                        <img src="Images/Java logo.webp" alt="C++"  />
+                        <h5>C++</h5>
+                    </div>
+
+                    <div className="Skills-icons" title="MySQL">
+                        <img src="Images/Java logo.webp" alt="MySQL"  />
+                        <h5>MySQL</h5>
+                    </div>
+
+                    
+                
+
+                </div>
+                
+
+            </section>
+            <section>
+                <h1>web development</h1>
+                <br />
+                <br />
+                <div className="icons-container">
+                    <div className="Skills-icons" title="HTML">
+                        <img src="Images/Java logo.webp" alt="HTML"  />
+                        <h5>HTML</h5>
+                    </div>
+
+                    <div className="Skills-icons" title="CSS">
+                        <img src="Images/Java logo.webp" alt="CSS"  />
+                        <h5>CSS</h5>
+                    </div>
+
+                    <div className="Skills-icons" title="React">
+                        <img src="Images/Java logo.webp" alt="React"  />
+                        <h5>React</h5>
+                    </div>
+
+                </div>
+                
+            </section>
+            <section>
+                <h1>tools</h1>
+                <br />
+                <br />
+                <div className="icons-container">
+                    <div className="Skills-icons" title="Git">
+                        <img src="" alt="Git" />
+                        <h5>Git</h5>
+                    </div>
+
+                    <div className="Skills-icons" title="GitHub">
+                        <img src="" alt="GitHub" />
+                        <h5>GitHub</h5>
+                    </div>
+
+                    <div className="Skills-icons" title="VS Code">
+                        <img src="" alt="VS Code" />
+                        <h5>VS Code</h5>
+                    </div>
+
+                    <div className="Skills-icons" title="Figma">
+                        <img src="" alt="Figma" />
+                        <h5>Figma</h5>
+                    </div>
+
+                </div>
+
+            </section>
         </div>
     );
     
