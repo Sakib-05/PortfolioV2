d23625e02909800adbf0fced8b53145d75dfe407
Sakib-05 <sakibimamul03@gmail.com>
2025-01-25 16:36:59 +0000

    fixing projects components continued

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,32 +2,37 @@ import React from "react";
 import Project from "./Project";
 
 const Projects = () => {
-  const projectData = [
+  const projectsData = [
     {
       image: "https://via.placeholder.com/300",
-      title: "Project 1",
+      title: "Movie Review App",
       description: "This is a description for Project 1.",
-      tech: ["React", "CSS", "JavaScript"],
+      tech: ["React", "HTML","CSS", "JavaScript", "TMDB API"],
     },
     {
       image: "https://via.placeholder.com/300",
-      title: "Project 2",
+      title: "Dentists Platform",
       description: "This is a description for Project 2.",
-      tech: ["HTML", "CSS", "Node.js"],
+      tech: ["HTML", "CSS", "JavaScript"],
     },
   ];
 
   return (
     <div className="projects-container">
-      {projectData.map((project, index) => (
-        <Project
-          key={index}
-          image={project.image}
-          title={project.title}
-          description={project.description}
-          tech={project.tech}
-        />
-      ))}
+        <h1 style={{justifySelf:"center"}}>Projects</h1>
+        <div style={{display:"flex"}}>
+            {projectsData.map((project, index) => (
+                <Project
+                key={index}
+                image={project.image}
+                title={project.title}
+                description={project.description}
+                tech={project.tech}
+                />
+            ))}
+
+        </div>
+      
     </div>
   );
 };
