61c6ff3524d0c1a4d09c88e6c5e5ef0cc28af533
Sakib-05 <sakibimamul03@gmail.com>
2025-02-09 21:52:43 +0000

    completed each project into interactive cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,42 +4,35 @@ import Project from "./Project";
 const Projects = () => {
   const projectsData = [
     {
-      image: "/Images/Cpp icon.png",
+      image: "/Images/ProjectsImages/movie review website project.png",
       title: "Movie Review App",
-      description: "This is a description for Project 1.",
-      tech: ["React", "HTML","CSS", "JavaScript", "TMDB API"],
+      description: "Developed a movie review web app using React and TMDB API.",
+      tech: ["React", "JavaScript", "TMDB API"],
     },
     {
-      image: "/Images/Cpp icon.png",
+      image: "/Images/ProjectsImages/dental healthcare platform.jpeg",
       title: "Dentists Platform",
-      description: "This is a description for Project 2.",
+      description: "A hackathon project that helps dentists manage their patients and communicate.",
       tech: ["HTML", "CSS", "JavaScript"],
     },
     {
-        image: "/Images/Cpp icon.png",
-        title: "Dentists Platform",
-        description: "This is a description for Project 2.",
-        tech: ["HTML", "CSS", "JavaScript"],
+        image: "/Images/ProjectsImages/java2DGame.png",
+        title: "2D Batman Game",
+        description: "a 2D game in Java as part of my university coursework.",
+        tech: ["Java"],
     },
     {
-        image: "/Images/Cpp icon.png",
-        title: "Dentists Platform",
-        description: "This is a description for Project 2.",
+        image: "/Images/ProjectsImages/oldPortfolioWbsite.jpeg",
+        title: "Previous Portfolio Website",
+        description: "My previous portfolio website that I developed using HTML, CSS, and JavaScript.",
         tech: ["HTML", "CSS", "JavaScript"],
-    },
-    {
-        image: "/Images/Cpp icon.png",
-        title: "Dentists Platform",
-        description: "This is a description for Project 2.",
-        tech: ["HTML", "CSS", "JavaScript"],
-    },
-      
+    },  
   ];
 
   return (
     <div className="Projects">
         <h1 style={{justifySelf:"center"}}>Projects</h1>
-        <div style={{display:"flex", flexWrap:"wrap", gap:"5px", justifyContent:"space-evenly"}}>
+        <div style={{display:"flex", flexWrap:"wrap", gap:"15px", justifyContent:"space-evenly"}}>
             {projectsData.map((project, index) => (
                 <Project
                 key={index}
