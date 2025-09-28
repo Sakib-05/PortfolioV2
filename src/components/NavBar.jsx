d8ada00ef55a002413533ade4318adc1dfb7f306
Sakib-05 <sakibimamul03@gmail.com>
2025-09-28 14:56:42 +0100

    fixed image directories

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,9 +7,9 @@ export default function NavBar() {
             <h2>MY PORTFOLIO</h2>
 
             <section>
-                <a href="/images/CV.pdf" target="_blank">
+                <a href="public/images/CV.pdf" target="_blank">
                     <div className="resume">
-                        <img src="Images/download icon.png" alt="download" id="download-icon" />
+                        <img src="public/Images/download icon.png" alt="download" id="download-icon" />
                         <p>resume</p>
                     </div>
                 </a>
