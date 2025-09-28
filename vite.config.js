f91b245b20bfdc1b9abcf166c72cf2250f9b0e3f
Sakib Imamul Hoque <sakibimamul03@gmail.com>
2025-09-28 15:10:16 +0100

    Fix GitHub Pages deployment by updating vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolioV2/'
+  base: '/portfolioV2/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 });
